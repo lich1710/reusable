fb1d67293912def8a52e0ccbc520e2979c728568
Lich Nguyen <w00f@iMac.local>
2018-03-04 22:41:51 +0800

    Successful Run WebCluster

--- modules/web-cluster/outputs.tf
+++ modules/web-cluster/outputs.tf
@@ -1,7 +1,7 @@
 output "asg_name" {
-  value = "aws_autoscaling_group.cluster.name"
+  value = "${aws_autoscaling_group.cluster.name}"
 }
 
 output "elb_dns_name" {
-  value = "aws_elb.elb.dns_name"
+  value = "${aws_elb.elb.dns_name}"
 }
